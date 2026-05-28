d2d5ab4fee27f88c6841902482743cc61b0b8a69
joseph meneses <joseph.menesess@uniagustiniana.edu.co>
2026-05-28 06:14:52 -0500

    Add subtraction multiplication and division operations

--- src/utils/operaciones.py
+++ src/utils/operaciones.py
@@ -1,2 +1,11 @@
 def sumar(a, b):
     return a + b
+def restar(a, b):
+    return a - b 
+def multiplicar(a, b):
+    return a * b
+def dividir(a, b):                  
+    if b != 0:
+        return a / b
+    else:
+        return "Error: No se puede dividir por cero" 
--- tests/test_operaciones.py
+++ tests/test_operaciones.py
@@ -1,4 +1,21 @@
-from src.utils.operaciones import sumar
+from src.utils.operaciones import (
+    sumar,
+    restar,
+    multiplicar,
+    dividir
+)
 
 def test_sumar():
     assert sumar(2, 3) == 5
+
+def test_restar():
+    assert restar(5, 2) == 3
+
+def test_multiplicar():
+    assert multiplicar(4, 3) == 12
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+
+def test_dividir_por_cero():
+    assert dividir(10, 0) == "Error: No se puede dividir por cero"
